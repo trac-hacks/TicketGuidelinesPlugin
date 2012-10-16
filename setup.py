7bc8c44c535c62981ed57df3efdcf49ddf5e66f3
Greg Lavallee <greglavallee@wapolabs.com>
2012-10-16 15:17:20 -0400

    Moving things around

--- setup.py
+++ setup.py
@@ -1,29 +1,29 @@
 from setuptools import setup
 
 PACKAGE = 'TicketGuidelinesPlugin'
-VERSION = '0.1'
+VERSION = '0.2'
 
 setup(
     name = PACKAGE,
     version = VERSION,
-    
-    author = "Sebastian Krysmanski",
+
+    author = "Greg Lavallee (forked from Sebastian Krysmanski)",
     author_email = None,
-    url = "https://svn.mayastudios.de/mtpp/wiki/Plugins/TicketGuidelinesPlugin",
-    
+    url = "https://github.com/elgreg/TicketGuidelinesPlugin",
+
     description = "Adds your ticket guidelines to the ticket view. The guidelines " \
                   "are specified in the wiki pages 'TicketGuidelines/NewShort' and " \
-                  "'TicketGuidelines/ModifyShort'.",
+                  "'TicketGuidelines/ModifyShort'. Forked to add js",
     keywords = "trac plugins",
-    
+
     license = "Modified BSD",
-    
+
     install_requires = [
         'Trac>=0.11',
     ],
-    
+
     packages = ['ticketguidelines'],
     package_data = { 'ticketguidelines': [ 'htdocs/*' ] },
-                                     
+
     entry_points = { 'trac.plugins': '%s = ticketguidelines.web_ui' % PACKAGE },
 )
