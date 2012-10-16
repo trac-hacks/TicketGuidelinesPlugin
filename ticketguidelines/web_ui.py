b67e55d129a97a884c29ea44f8c3b42c2ef5c0a6
Greg Lavallee <greglavallee@wapolabs.com>
2012-10-16 16:22:48 -0400

    Prototype for inserting default text - this should obviously be done in a wiki template somewhere or in the ini somehow.

--- ticketguidelines/web_ui.py
+++ ticketguidelines/web_ui.py
@@ -5,11 +5,11 @@
 #                         All rights reserved.
 #
 ################################################################################
-# 
+#
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
 # are met:
-# 
+#
 # 1. Redistributions of source code must retain the above copyright
 #    notice, this list of conditions and the following disclaimer.
 # 2. Redistributions in binary form must reproduce the above copyright
@@ -19,7 +19,7 @@
 # 3. The name of the author may not be used to endorse or promote
 #    products derived from this software without specific prior
 #    written permission.
-# 
+#
 # THIS SOFTWARE IS PROVIDED BY THE AUTHOR `AS IS'' AND ANY EXPRESS
 # OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
 # WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -33,7 +33,7 @@
 # SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
 ################################################################################
-# 
+#
 # This software consists of voluntary contributions made by many
 # individuals. For the exact contribution history, see the revision
 # history and logs, available at: https://svn.mayastudios.de/mtpp/log/
@@ -50,7 +50,7 @@
 
 from trac.core import *
 from trac.web.api import IRequestFilter, ITemplateStreamFilter
-from trac.web.chrome import add_stylesheet, ITemplateProvider
+from trac.web.chrome import add_script, add_stylesheet, ITemplateProvider
 
 from trac.wiki.model import WikiPage
 from trac.wiki.formatter import format_to_html
@@ -60,6 +60,7 @@ from genshi.filters.transform import Transformer
 
 NEW_TICKET_PAGE_NAME = 'TicketGuidelines/NewTicketSummary'
 MODIFY_TICKET_PAGE_NAME = 'TicketGuidelines/ModifyTicketSummary'
+
 
 def _get_wiki_html(env, data, is_new_ticket):
   wiki_page = is_new_ticket and NEW_TICKET_PAGE_NAME or MODIFY_TICKET_PAGE_NAME
@@ -75,18 +76,18 @@ def _get_wiki_html(env, data, is_new_ticket):
       text = 'No ticket guidelines for modifying a ticket have been specified. ' \
              'Please create [wiki:%s this wiki page] to specify these guidelines.' \
              % MODIFY_TICKET_PAGE_NAME
-  
+
   return tag.div(format_to_html(env, data['context'], text), class_='ticket-guidelines')
 
 class TicketGuidelinesContentProvider(Component):
   """ This component provides the CSS file for the ticket guidelines box. """
 
   implements(ITemplateProvider, IRequestFilter)
-  
+
   # ITemplateProvider methods
   def get_templates_dirs(self):
     """Return a list of directories containing the provided template files.
-    """  
+    """
     return []
 
   def get_htdocs_dirs(self):
@@ -96,20 +97,20 @@ class TicketGuidelinesContentProvider(Component):
         Each item in the list must be a `(prefix, abspath)` tuple. The
         `prefix` part defines the path in the URL that requests to these
         resources are prefixed with.
-      
+
         The `abspath` is the absolute path to the directory containing the
         resources on the local file system.
     """
     from pkg_resources import resource_filename
     return [('ticketguidelinesplugin', resource_filename(__name__, 'htdocs'))]
-    
+
   # IRequestFilter methods
-  
+
   def post_process_request(self, req, template, data, content_type):
     """Do any post-processing the request might need; typically adding
     values to the template `data` dictionary, or changing template or
     mime type.
-    
+
     `data` may be update in place.
 
     Always returns a tuple of (template, data, content_type), even if
@@ -121,29 +122,30 @@ class TicketGuidelinesContentProvider(Component):
 
     (Since 0.11)
     """
-    
+
     if template and (req.path_info.startswith('/ticket/') or req.path_info.startswith('/newticket')):
       add_stylesheet(req, 'ticketguidelinesplugin/main.css')
-      
+      add_script(req, 'ticketguidelinesplugin/script.js')
+
     return (template, data, content_type)
 
   def pre_process_request(self, req, handler):
     """Called after initial handler selection, and can be used to change
     the selected handler or redirect request.
-    
+
     Always returns the request handler, even if unchanged.
     """
     return handler
-    
+
 class NewTicketGuidelinesBox(Component):
-  """ This component inserts the ticket guidelines hints for new into the 
-      ticket form. 
+  """ This component inserts the ticket guidelines hints for new into the
+      ticket form.
   """
 
   implements(ITemplateStreamFilter)
-  
+
   # ITemplateStreamFilter methods
-    
+
   def filter_stream(self, req, method, filename, stream, data):
     """Return a filtered Genshi event stream, or the original unfiltered
     stream if no match.
@@ -157,21 +159,21 @@ class NewTicketGuidelinesBox(Component):
     """
     if method != 'xhtml':
       return stream
-      
+
     if req.path_info.startswith('/newticket'):
       stream = stream | Transformer('//form[@id="propertyform"]').before(_get_wiki_html(self.env, data, True))
-      
+
     return stream
 
 class TicketCommentGuidelinesBox(Component):
-  """ This component inserts the ticket guidelines hints for ticket comments 
-      into the ticket form. 
+  """ This component inserts the ticket guidelines hints for ticket comments
+      into the ticket form.
   """
 
   implements(ITemplateStreamFilter)
-  
+
   # ITemplateStreamFilter methods
-    
+
   def filter_stream(self, req, method, filename, stream, data):
     """Return a filtered Genshi event stream, or the original unfiltered
     stream if no match.
@@ -185,8 +187,8 @@ class TicketCommentGuidelinesBox(Component):
     """
     if method != 'xhtml':
       return stream
-      
+
     if req.path_info.startswith('/ticket/'):
       stream = stream | Transformer('//form[@id="propertyform"]').before(_get_wiki_html(self.env, data, False))
-      
+
     return stream
