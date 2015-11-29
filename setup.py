13f5d8f841bcabc7829f9970676708022745ae6c
Ryan J Ollos <ryanollos@verasonics.com>
2015-11-29 15:32:21 -0800

    0.3dev: Cleanup code.

--- setup.py
+++ setup.py
@@ -1,29 +1,31 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2009-2010 Sebastian Krysmanski
+# Copyright (C) 2012 Greg Lavallee
+# All rights reserved.
+#
+# This software is licensed as described in the file COPYING, which
+# you should have received as part of this distribution.
+#
+
 from setuptools import setup
 
 PACKAGE = 'TicketGuidelinesPlugin'
 VERSION = '0.3'
 
 setup(
-    name = PACKAGE,
-    version = VERSION,
-
-    author = "Greg Lavallee (forked from Sebastian Krysmanski)",
-    author_email = None,
-    url = "https://github.com/elgreg/TicketGuidelinesPlugin",
-
-    description = "Adds your ticket guidelines to the ticket view. The guidelines " \
-                  "are specified in the wiki pages 'TicketGuidelines/NewShort' and " \
-                  "'TicketGuidelines/ModifyShort'. Forked to add js",
-    keywords = "trac plugins",
-
-    license = "Modified BSD",
-
-    install_requires = [
-        'Trac>=0.11',
-    ],
-
-    packages = ['ticketguidelines'],
-    package_data = { 'ticketguidelines': [ 'htdocs/*' ] },
-
-    entry_points = { 'trac.plugins': '%s = ticketguidelines.web_ui' % PACKAGE },
+    name=PACKAGE,
+    version=VERSION,
+    author='Sebastian Krysmanski',
+    url='https://github.com/trac-hacks/TicketGuidelinesPlugin',
+    description="Adds your ticket guidelines to the ticket view. The "
+                "guidelines are specified in the wiki pages "
+                "'TicketGuidelines/NewShort' and "
+                "'TicketGuidelines/ModifyShort'.",
+    keywords='trac plugin',
+    license='Modified BSD',
+    install_requires=['Trac'],
+    packages=['ticketguidelines'],
+    package_data={'ticketguidelines': ['htdocs/*']},
+    entry_points={'trac.plugins': '%s = ticketguidelines.web_ui' % PACKAGE},
 )
--- ticketguidelines/__init__.py
+++ ticketguidelines/__init__.py
@@ -1,2 +1,2 @@
-# This file is necessary for Trac to be able to load this plugin. It doesn't 
-# need to contain any code.
+import pkg_resources
+pkg_resources.require('Trac >= 0.11')
